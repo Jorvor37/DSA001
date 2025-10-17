a9a1458592693445416eefa89d2fc9531817cc9f
JorVor <129814086+Jorvor37@users.noreply.github.com>
2025-10-17 13:43:56 +0700

    Runtime 0 ms Beats 100.00% Memory 17.59 MB Beats 96.78%

--- LeetCode1732.FindtheHighestAltitude.py
+++ LeetCode1732.FindtheHighestAltitude.py
@@ -1,3 +1,12 @@
+#sceond attempt(better)
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        gain.insert(0, 0)
+        for i in range(1, len(gain)):
+            gain[i] = gain[i-1]+gain[i]
+        return max(gain)
+
+#first attempt
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         altitudes = []
